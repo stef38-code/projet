cf1d49f9ec1f0ef51188bb25dc305f5e32da681d
stephane <stephane.betton@gmail.com>
2022-11-30 18:33:37 +0100

    feat: Nir informations
    * add doc

--- business/src/main/java/org/hussard/business/tools/nss/elements/ElementFactory.java
+++ business/src/main/java/org/hussard/business/tools/nss/elements/ElementFactory.java
@@ -1,10 +1,23 @@
 package org.hussard.business.tools.nss.elements;
 
+/**
+ * The type Element factory.
+ */
 public class ElementFactory {
+    /**
+     * Instantiates a new Element factory.
+     */
     private ElementFactory() {
         throw new UnsupportedOperationException("ElementFactory is a utility class and cannot be instantiated");
     }
-    public static Element get(EnumElement enumElement){
+
+    /**
+     * Get element.
+     *
+     * @param enumElement the enum element
+     * @return the new instance element
+     */
+    public static Element get(EnumElement enumElement) {
         return enumElement.getInstance();
     }
 }
--- business/src/main/java/org/hussard/business/tools/nss/elements/EnumElement.java
+++ business/src/main/java/org/hussard/business/tools/nss/elements/EnumElement.java
@@ -10,21 +10,58 @@ import org.hussard.business.tools.nss.elements.sexe.Sexe;
 
 import java.util.function.Supplier;
 
+/**
+ * The enum Enum element.
+ */
 public enum EnumElement {
+    /**
+     * Sexe enum element.
+     */
     SEXE(Sexe::new),
+    /**
+     * Annee enum element.
+     */
     ANNEE(Annee::new),
+    /**
+     * Mois enum element.
+     */
     MOIS(Mois::new),
+    /**
+     * Code enum element.
+     */
     CODE(Code::new),
+    /**
+     * Numero enum element.
+     */
     NUMERO(Numero::new),
+    /**
+     * Ordre enum element.
+     */
     ORDRE(Ordre::new),
+    /**
+     * Cle enum element.
+     */
     CLE(Cle::new);
 
+    /**
+     * The Instantiator.
+     */
     private final Supplier<Element> instantiator;
 
+    /**
+     * Instantiates a new Enum element.
+     *
+     * @param instantiator the instantiator
+     */
     EnumElement(Supplier<Element> instantiator) {
         this.instantiator = instantiator;
     }
 
+    /**
+     * Gets instance.
+     *
+     * @return the instance
+     */
     public Element getInstance() {
         return instantiator.get();
     }
